ad10d0b1b9a8fcd9e4a834475d0b00123f84a5e6
berenboden <beren.boden@gmail.com>
2023-01-15 11:27:21 +1300

    error fix

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
 function errorHandler(err, req, res, next) {
   if (err) {
-    console.log(err.message)
+    console.log(err)
     res.status(500).send({message: err.message});
   } else {
     next();
